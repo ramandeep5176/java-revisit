f8c7828c0bace1fcaf6c46a7f66a8785d4ed8825
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-12-11 11:21:44 -0500

    flip colors

--- app.js
+++ app.js
@@ -26,15 +26,37 @@
 //   console.log(amount);
 //   amount++;
 // }
+// const one = document.querySelector(".one");
+// const two = document.querySelector(".two");
+// const three = document.querySelectommr(".three");
+// const four = document.querySelector(".four");
+// const active = document.querySelectorAll(".active");
+// const na = document.getElementById("four");
+// four.addEventListener("click", toggle);
+// function toggle() {
+//   na.classList.toggle("active");
+// }
 
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const four = document.querySelector(".four");
-const active = document.querySelectorAll(".active");
-var na = document.getElementById( "four");
+// const colors = ["black", "green", "red"];
+// const two = document.getElementById("two");
 
-four.addEventListener("click", toggle);
-function toggle() { 
-  na.classList.toggle("active");
+// two.addEventListener("click", function () {
+//   const randomNumber = 2;
+//   document.two.textContent = two[randomNumber];
+// });
+
+const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const container = document.querySelector(".container");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNo();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNo() {
+  return Math.floor(Math.random() * colors.length);
 }
