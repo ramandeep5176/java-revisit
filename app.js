e079b4871796d8246d0ee57ac974e3ea8b16ea46
ramandeep5176 <ramandeepgill5176@gmail.com>
2022-01-05 18:36:27 -0500

    3-done-today

--- app.js
+++ app.js
@@ -93,74 +93,98 @@
 //   });
 // });
 
-const data = [
-  {
-    id: 1,
-    pic: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    name: "ram1",
-    job: "web1",
-    info: "#$1 #uwrhgfiuhrhfk bwiry iryhiuf iwuhiure fir igrh ih",
-  },
-  {
-    id: 2,
-    pic: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    name: "ram2",
-    job: "web2",
-    info: "#$2 #uwrhgfiuhrhfk bwirydi laudantium consequatur expedita asperioresolestias magndi laudantium consequatur expedita asperioresolestias magndi laudantium consequatur expedita asperioresolestias magndi laudantium consequatur expedita asperioresolestias magn iryhiuf iwuhiure fir igrh ih",
-  },
-  {
-    id: 3,
-    pic: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    name: "ram3",
-    job: "web3",
-    info: "#$3 di laudantium consequatur expedita asperioresolestias magn di laudantium consequatur expedita asperioresolestias magn #uwrhgfiuhrhfk bwiry iryhiuf iwuhiure fir igrh ih",
-  },
-];
+// const data = [
+//   {
+//     id: 1,
+//     pic: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+//     name: "ram1",
+//     job: "web1",
+//     info: "#$1 #uwrhgfiuhrhfk bwiry iryhiuf iwuhiure fir igrh ih",
+//   },
+//   {
+//     id: 2,
+//     pic: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+//     name: "ram2",
+//     job: "web2",
+//     info: "#$2 #uwrhgfiuhrhfk bwirydi laudantium consequatur expedita asperioresolestias magndi laudantium consequatur expedita asperioresolestias magndi laudantium consequatur expedita asperioresolestias magndi laudantium consequatur expedita asperioresolestias magn iryhiuf iwuhiure fir igrh ih",
+//   },
+//   {
+//     id: 3,
+//     pic: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+//     name: "ram3",
+//     job: "web3",
+//     info: "#$3 di laudantium consequatur expedita asperioresolestias magn di laudantium consequatur expedita asperioresolestias magn #uwrhgfiuhrhfk bwiry iryhiuf iwuhiure fir igrh ih",
+//   },
+// ];
 
-// selection
-const pic = document.querySelector(".pic");
-const name = document.querySelector(".name");
-const job = document.querySelector(".job");
-const info = document.querySelector(".info");
-// btns
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const surprise = document.querySelector(".surprise");
+// // selection
+// const pic = document.querySelector(".pic");
+// const name = document.querySelector(".name");
+// const job = document.querySelector(".job");
+// const info = document.querySelector(".info");
+// // btns
+// const left = document.querySelector(".left");
+// const right = document.querySelector(".right");
+// const surprise = document.querySelector(".surprise");
 
-let current = 0;
+// let current = 0;
 
-window.addEventListener("DOMContentLoaded", function () {
-  showData(current);
-});
+// window.addEventListener("DOMContentLoaded", function () {
+//   showData(current);
+// });
 
-function showData(mainData) {
-  const datacopy = data[mainData];
-  job.textContent = datacopy.job;
-  name.textContent = datacopy.name;
-  info.textContent = datacopy.info;
-  pic.src = datacopy.pic;
-}
+// function showData(mainData) {
+//   const datacopy = data[mainData];
+//   job.textContent = datacopy.job;
+//   name.textContent = datacopy.name;
+//   info.textContent = datacopy.info;
+//   pic.src = datacopy.pic;
+// }
 
-right.addEventListener("click", function () {
-  current++;
+// right.addEventListener("click", function () {
+//   current++;
 
-  //
-  if (current > data.length - 1) {
-    current = 0;
-  }
-  showData(current);
-  //
-});
-left.addEventListener("click", function () {
-  current--;
-  //
-  if (current < 0) {
-    current = data.length - 1;
-  }
+//   //
+//   if (current > data.length - 1) {
+//     current = 0;
+//   }
+//   showData(current);
+//   //
+// });
+// left.addEventListener("click", function () {
+//   current--;
+//   //
+//   if (current < 0) {
+//     current = data.length - 1;
+//   }
 
-  showData(current);
-});
-surprise.addEventListener("click", function () {
-  current = Math.floor(Math.random() * data.length);
-  showData(current);
-});
+//   showData(current);
+// });
+// surprise.addEventListener("click", function () {
+//   current = Math.floor(Math.random() * data.length);
+//   showData(current);
+// });
+
+// const toggle = document.querySelector(".toggle");
+// const hide = document.querySelector(".hide");
+// const hideToggle = document.querySelector(".hide-toggle");
+
+// toggle.addEventListener("click", function () {
+//   if (hide.classList.contains("hide")) {
+//     hide.classList.toggle("hide");
+//   } else {
+//     hide.classList.toggle("hide");
+//   }
+// });
+// toggle.addEventListener("click", function () {
+//   hide.classList.toggle("hide");
+// });
+
+// const toggle = document.querySelector(".toggle");
+// const basicOff = document.querySelector(".basic");
+// const modalOff = document.querySelector(".modal");
+
+// toggle.addEventListener("click", function () {
+//   basicOff.classList.toggle("basic-off");
+//   modalOff.classList.toggle("modal-off");
+// });
